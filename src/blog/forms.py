a6c8637e229b0363c53f9d8b96263f41aa8d57c0
rooneyrulz <roony0782045336@gmail.com>
2019-09-07 02:21:43 +0530

    added some form widget

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -4,6 +4,45 @@ from .models import Blog
 
 
 class BlogForm(forms.ModelForm):
+	title = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+				'id': 'tile',
+				'class': 'form-control form-control-lg',
+				'placeholder': 'Enter title'
+			}
+		)
+	)
+
+	description = forms.CharField(
+		widget=forms.Textarea(
+			attrs={
+				'id': 'description',
+				'class': 'form-control form-control-lg',
+				'placeholder': 'Enter description',
+				'rows': 4
+			}
+		)
+	)
+
+	author = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+				'id': 'author',
+				'class': 'form-control form-control-lg',
+				'placeholder': 'Enter author'
+			}
+		)
+	)
+
+	# date = forms.DateTimeField(
+	# 	widget=forms.DateTimeField(
+	# 		attrs={
+	# 			'id': 'date',
+	# 			'class': 'form-control form-control-lg'
+	# 		}
+	# 	)
+	# )
 
 	class Meta:
 		model = Blog
