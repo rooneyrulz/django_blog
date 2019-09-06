8df06503bf7b5a5df1fb6f422fbd070be9aa6b8c
rooneyrulz <roony0782045336@gmail.com>
2019-09-07 00:26:22 +0530

    modified urls

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import blog_home_view, blog_about_view
+from .views import blog_home_view, blog_list_view, blog_create_view, blog_details_view, blog_about_view
 
 app_name = 'blog'
 urlpatterns = [
-	path('', blog_home_view, name='blog_home_view'),
+	path('home/', blog_home_view, name='blog_home_view'),
+	path('list/', blog_list_view, name='blog_list_view'),
+	path('create/', blog_create_view, name='blog_create_view'),
+	path('details/<int:id>/', blog_details_view, name='blog_details_view'),
 	path('about/', blog_about_view, name='blog_about_view')
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -6,6 +6,21 @@ def blog_home_view(request):
 	return render(request, 'blog/blog_home.html', {})
 
 
+# Blog List View
+def blog_list_view(request):
+	return render(request, 'blog/blog_list.html', {})
+
+
+# Blog Create View
+def blog_create_view(request):
+	return render(request, 'blog/blog_create.html', {})
+
+
+# Blog Details View
+def blog_details_view(request, id):
+	return render(request, 'blog/blog_details.html', {'id': id})
+
+
 # Blog About View
 def blog_about_view(request):
 	return render(request, 'blog/blog_about.html', {})
